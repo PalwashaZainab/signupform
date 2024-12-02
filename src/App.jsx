f388abfc3cf60d434bb8a85067b49db678384b77
PalwashaZainab <zehrazainab889@gmail.com>
2024-12-02 14:46:42 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState("")
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const phone = event.target.phone.value;
+    axios.post("https://jsonplaceholder.typicode.com/posts", {
+      name,
+      email,
+      phone,
+    }).then(response => {
+      console.log(response);
+      event.target.reset()
+      setMessage("User " + name + " has been added successfully");
+    }).catch(error => {
+      console.error(error);
+    })
+  };
+
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="signup-form">
+        <input
+          type="text"
+          placeholder="Name"
+          name='name'
+          className="form-input"
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          name='email'
+          className="form-input"
+          required
+        />
+        <input
+          type="text"
+          placeholder="Phone"
+          name='phone'
+          className="form-input"
+          required
+        />
+        <button type="submit" className="form-button">Add User</button>
+      </form>
+        <p>{message}</p>
+
+    </>
+  )
+}
+
+export default App
